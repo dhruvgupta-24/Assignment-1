fa1a011bfef3974cbb9420eb373ef00c96d8441d
Dhruv Gupta <dgupta6_be23@thapar.edu>
2024-01-19 19:20:09 +0530

    upload

---
+++ Assignment 2/console.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+
+    printf("Hello World");
+    printf("Hello World\n");
+    printf("Hello World\t");
+    printf("Hello World\b");
+    printf("Hello World");
+    
+    return 0;
+}
