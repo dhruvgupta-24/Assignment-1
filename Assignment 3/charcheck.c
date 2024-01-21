02b008e1ad14f6c6f2931eec45ccccc209bf981b
Dhruv Gupta <dgupta6_be23@thapar.edu>
2024-01-21 14:08:59 +0530

    upload

---
+++ Assignment 3/charcheck.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+
+    char ch;
+    printf("Enter a character: ");
+    scanf("%c",&ch);
+
+    if(ch>='A' && ch<='Z'){
+        printf("%c is a CAPITAL letter",ch);
+    } else if(ch>='a' && ch<='z'){
+        printf("%c is a small letter",ch);
+    } else if(ch>='0' && ch<='9'){
+        printf("%c is a digit",ch);
+    } else{
+        printf("%c is a special symbol",ch);
+    }
+    
+    return 0;
+}
