f69f3f71ce80021429b83c15440928e3f1d3a422
Dhruv Gupta <dgupta6_be23@thapar.edu>
2024-01-22 08:27:56 +0530

    upload

--- Assignment 3/loops.c
+++ Assignment 3/loops.c
@@ -7,14 +7,14 @@ int main(){
     printf("Enter a number: ");
     scanf("%d",&a);
 
-    // Factorial
+    // Factorial of number
     int fac=1;
     for(int i=1;i<=a;i++){
         fac=fac*i;
     } 
     printf("Factorial of %d = %d\n",a,fac);
 
-    // Prime or not
+    // Whether no. is prime or not
     int i;
     for(i=2;i<a;i++){
         if(a%i==0){
@@ -22,24 +22,27 @@ int main(){
             break;
         } 
     } if(i==a){
-        printf("%d is a prime number",a);
+        printf("%d is a prime number\n",a);
     }
 
     // X raised to power n (DOUBT)
 
-    // Armstrong Number
-    int b,c,d,e;
-    printf("Specify the number of digits in your number: ");
-    scanf("%d",&b);
+    // Armstrong numbers between 1 to 500 (DOUBT)
 
-    for(i=1;i<=500;i++){
-        c=i%10;
-        d=i%100/10;
-        e=i%1000/100;
-        if(pow(c,b)+pow(d,b)+pow(e,b)==a){
-            printf("%d\n",a);
-        }
+    // Range of set of entered nos. by finding smallest and largest.
+
+    // Multiplication table
+    int mul;
+    for(i=0;i<=10;i++){
+        mul=a*i;
+        printf("%d x %d = %d\n",a,i,mul);
     }
 
+    // HCF of two numbers (DOUBT)
+
+    // GCD of two numbers (DOUBT)
+
+    // Generate Fibonacci series (DOUBT)
+    
     return 0;
 }
