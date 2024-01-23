d75577abd54995a8623f2b19bb14709f6237c414
Dhruv Gupta <dgupta6_be23@thapar.edu>
2024-01-23 10:01:27 +0530

    upload

--- Assignment 2/quadratic.c
+++ Assignment 2/quadratic.c
@@ -14,8 +14,8 @@ int main (){
     d=(b*b)-(4*a*c);
     x=(-b+sqrt(d))/(2*a);
     y=(-b-sqrt(d))/(2*a);
-    printf("1st root of quadratic equation (%f)x^2+(%f)x+(%f)=%f\n",a,b,c,x);
-    printf("2nd root of quadratic equation (%f)x^2+(%f)x+(%f)=%f",a,b,c,y);
+    printf("1st root of quadratic equation (%.2f)x^2+(%.2f)x+(%.2f)=%.3f\n",a,b,c,x);
+    printf("2nd root of quadratic equation (%.2f)x^2+(%.2f)x+(%.2f)=%.3f",a,b,c,y);
 
     return 0;
 }
