6afd8a861da04074dc18f7b973a6bd5e9868ce08
Dhruv Gupta <dgupta6_be23@thapar.edu>
2024-01-23 09:59:50 +0530

    upload

--- Assignment 2/functions.c
+++ Assignment 2/functions.c
@@ -3,11 +3,27 @@
 
 int main(){
 
-   int age;
-   printf("Enter your age: ");
-   scanf("%d",&age);
+    float a,b,c,d,e,f;
+    int g;
+    printf("Enter a number you want sin of(in rad): ");
+    scanf("%f",&a);
+    printf("Sin(%.3f) = %.3f\n",a,sin(a));
 
-   age>=18 ? printf("You can drive"):printf("You cannot drive");
+    printf("Enter a number you want cos of(in rad): ");
+    scanf("%f",&b);
+    printf("Cos(%.3f) = %.3f\n",b,cos(b));
+
+    printf("Enter a number you want log of: ");
+    scanf("%f",&c);
+    printf("Log(%.3f) = %.3f\n",c,log(c));
+
+    printf("Enter a number you want and its power: ");
+    scanf("%f %d",&d,&g);
+    printf("%.2f^%d = %.2f\n",d,g,pow(d,g));
+
+    printf("Enter a number you want root of: ");
+    scanf("%f",&e);
+    printf("Root of %.2f = %.2f",e,sqrt(e));
 
     return 0;
 }
