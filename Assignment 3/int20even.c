716a18e7590f94a56e120471f2bf7cb7cee9bdeb
Dhruv Gupta <dgupta6_be23@thapar.edu>
2024-01-23 09:25:28 +0530

    upload

---
+++ Assignment 3/int20even.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+
+    int a;
+    for(int i=1;i<=20;i++){
+        printf("Enter a integer: ");
+        scanf("%d",&a);
+        // Count even integers (DOUBT)
+    }
+
+    return 0;
+}
